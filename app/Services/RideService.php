5ddddc6b3b25322ea6486de26ff43e2c49f91b67
Łukasz Woźnica <lukasz.l.woznica@gmail.com>
2020-09-18 15:54:22 +0200

    Check the rides started on the previous day

--- app/Services/RideService.php
+++ app/Services/RideService.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Str;
 
 class RideService
 {
-    public function getRidesByLocations(Location $startLocation, Location $endLocation, string $departureDate): Collection
+    public function getRidesByLocations(Location $startLocation, Location $endLocation, string $depDate): Collection
     {
         $startLocationId = $startLocation->id;
         $endLocationId = $endLocation->id;
-        $dayName = Str::lower(Carbon::parse($departureDate)->dayName);
+        $dayName = Str::lower(Carbon::parse($depDate)->dayName);
 
         $routes = Route::whereHas('locations', function (Builder $query) use ($startLocationId) {
             $query->where('id', $startLocationId);
@@ -38,34 +38,53 @@ class RideService
             ->toArray();
 
         $departureTimeSubquery = DB::table('location_route')
-            ->select('minutes_from_departure')
-            ->whereColumn('route_id', 'rides.route_id')
-            ->whereRaw('location_id = ?')
-            ->toSql();
+            ->whereRaw('location_id = ?', [$startLocationId]);
 
-        $departureTimeSubquery = "(departure_time + INTERVAL ($departureTimeSubquery) MINUTE)";
+        $calculatedDepartureTime = 'departure_time + INTERVAL (start_location.minutes_from_departure) MINUTE';
+        $select = "rides.*, $calculatedDepartureTime as start_location_dep_time";
 
-        $ridesQuery = Ride::whereHas('route', function (Builder $query) use ($routes) {
-            $query->whereIn('id', $routes);
-        })->where(function (Builder $query) use ($dayName, $departureDate) {
-            $query->whereHas('schedule', function (Builder $query) use ($dayName, $departureDate) {
-                $query->where($dayName, true)
-                    ->where('start_date', '<=', $departureDate)
-                    ->where(function (Builder $query) use ($departureDate) {
-                        $query->where('end_date', '>=', $departureDate)
+        $ridesQuery = Ride::selectRaw($select)
+            ->joinSub($departureTimeSubquery, 'start_location', function ($join) {
+                $join->on('rides.route_id', 'start_location.route_id');
+            })->whereHas('route', function (Builder $query) use ($routes) {
+                $query->whereIn('id', $routes);
+            })->where(function (Builder $query) use ($dayName, $depDate, $calculatedDepartureTime) {
+                $query->whereHas('schedule', function (Builder $query) use ($dayName, $depDate, $calculatedDepartureTime) {
+                    $query->where(function (Builder $query) use ($dayName, $depDate) {
+                        $query->where($dayName, true)
+                            ->where('start_date', '<=', $depDate);
+                    })->orWhere(function (Builder $query) use ($depDate, $calculatedDepartureTime) {
+                        $dayBefore = Carbon::parse($depDate)->subDay()->toDateString();
+                        $dayNameBefore = Str::lower(Carbon::parse($dayBefore)->dayName);
+                        $query->where($dayNameBefore, true)
+                            ->where('start_date', '<=', $dayBefore)
+                            ->whereRaw("$calculatedDepartureTime > ?", ['23:59:59']);
+                    })->where(function (Builder $query) use ($depDate) {
+                        $query->where('end_date', '>=', $depDate)
                             ->orWhereNull('end_date');
                     });
-            })->orWhere('ride_date', $departureDate);
-        })->with('route.locations');
+                })->orWhere(function (Builder $query) use ($depDate, $calculatedDepartureTime) {
+                    $query->where('ride_date', $depDate)
+                        ->orWhere(function (Builder $query) use ($depDate, $calculatedDepartureTime) {
+                            $dayBefore = Carbon::parse($depDate)->subDay()->toDateString();
+                            $query->where('ride_date', $dayBefore)
+                                ->whereRaw("$calculatedDepartureTime > ?", ['23:59:59']);
+                        });
+                });
+            })->with('route.locations');
 
-        if (Carbon::parse($departureDate)->isToday()) {
-            $ridesQuery->whereRaw("$departureTimeSubquery BETWEEN ? AND ?", [
-                $startLocationId,
+        if (Carbon::parse($depDate)->isToday()) {
+            $ridesQuery->whereRaw("$calculatedDepartureTime BETWEEN ? AND ?", [
                 Carbon::now()->toTimeString(),
                 '23:59:59'
             ]);
         }
 
-        return $ridesQuery->get();
+        $rides = $ridesQuery->get();
+        $rides = $rides->each(function ($ride) {
+            $ride->start_location_dep_time = Carbon::parse($ride->start_location_dep_time)->toTimeString();
+        });
+
+        return $rides->sortBy('start_location_dep_time');
     }
 }
